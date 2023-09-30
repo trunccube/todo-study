586288b999d4af42a6995ad043f87c38cfe49ab0
Trunccube <105231945+trunccube@users.noreply.github.com>
2023-09-30 16:06:38 +0000

    finish all

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    const [inputValue, setInputValue] = useState('');
+    const [infoBars, setInfoBars] = useState([]);
+
+    useEffect(() => {
+        const storedContent = JSON.parse(localStorage.getItem('infoBars') || '[]');
+        setInfoBars(storedContent);
+    }, []);
+
+    const handleAddContent = () => {
+        if (inputValue.trim() === '') {
+            alert('输入内容');
+            return;
+        }
+        
+        const newBars = [...infoBars, { content: inputValue, isChecked: false }];
+        setInfoBars(newBars);
+        localStorage.setItem('infoBars', JSON.stringify(newBars));
+        setInputValue('');
+    };
+
+    const handleToggleCheck = (index) => {
+        const newBars = [...infoBars];
+        newBars[index].isChecked = !newBars[index].isChecked;
+        setInfoBars(newBars);
+        localStorage.setItem('infoBars', JSON.stringify(newBars));
+    };
+
+    return (
+        <div className="container">
+            <div className="inputRow">
+                <input 
+                    type="text" 
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    placeholder="内容"
+                />
+                <button onClick={handleAddContent}>生成</button>
+            </div>
+            <div className="actionRow">
+                {infoBars.map((bar, index) => (
+                    <InfoBar 
+                        key={index} 
+                        data={bar} 
+                        onToggle={() => handleToggleCheck(index)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function InfoBar({ data, onToggle }) {
+    return (
+        <div 
+            className={`infoBar ${data.isChecked ? 'checked' : ''}`} 
+            onClick={onToggle}
+        >
+            <span className="infoContent">{data.content}</span>
+            <div className="checkbox"></div>
+        </div>
+    );
 }
 
 export default App;
